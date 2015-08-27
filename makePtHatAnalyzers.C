10663c09a19a480110387330524e3cf5f21e7c27
Zach Miller <zaglamir@gmail.com>
2015-08-27 10:56:27 -0500

    Add comments to makePtHatAnalyzer

--- makePtHatAnalyzers.C
+++ makePtHatAnalyzers.C
@@ -1,15 +1,33 @@
+/*
+// This creates a directory ("folder" input option) and then generates all of the 
+// required elements to run the online ptHatTemplates Monte Carlo. 
+// www.github.com//zaaglamir/pythiaNPEh contains the code for the online portion. 
+// The online portion requires a card (to feed options to Pythia), a script (to set the environment 
+// and run the .cpp that generates the events using TPythia), and a .job file which is 
+// used by condor (RCF) to submit many runs at once. 
+//
+// All required elements are created and put in an aptly named directory underneat/h the user
+// specified "folder" name. 
+//
+// Then use scp to move to RCF for use.
+//
+// Z. Miller - 8/27/15
+*/
 
+// Initialize each method
 Int_t makeScripts(Int_t,Int_t,Int_t,Int_t,const char*);
 Int_t makeCards(Int_t,Float_t,Float_t,Int_t,const char*);
 Int_t makeJobs(Int_t,Int_t,Int_t,Int_t,const char*);
 
 void makePtHatAnalyzers(Int_t mode = 1, Int_t pthatmin = 0, Int_t pthatmax = 100,Int_t maxRuns = 50,const char* folder="recent")
 {
+  // Check for valid mode 1 = CC, 2 = BB
   if(mode < 1 || mode > 2)
     {
       cout << "Not a valid mode. Choose 1=ccbar, or 2==bbar\n";
       exit(1);
     }
+  
   /// Make the directory system
   gSystem->mkdir(folder);
   char temp[100];
@@ -20,17 +38,19 @@ void makePtHatAnalyzers(Int_t mode = 1, Int_t pthatmin = 0, Int_t pthatmax = 100
   sprintf(temp,"%s/jobs",folder);
   gSystem->mkdir(temp);
 
-  // Create the files (cards, job list, execution scripts)
+  // Create the files (cards, job list, execution scripts) using
+  // each function.
   Int_t ms=0,mc=0,mj=0;
   ms = makeScripts(mode,pthatmin,pthatmax,maxRuns,folder);
   mc = makeCards(mode,(Float_t)pthatmin,(Float_t)pthatmax,maxRuns,folder);
   mj = makeJobs(mode,pthatmin,pthatmax,maxRuns,folder);
 
+  // Check for proper completion and print Success or Fail
   if(ms && mc && mj)
-    cout << "\nAnalyzer succesfully created\n"
+    cout << "\n!! Analyzer succesfully created. !!\n"
 	 << "move to RCF with: scp filename zamiller@rftpexp.rhic.bnl.gov:/star/u/zamiller/directory\n" << endl;
   else
-    cout << "\nFailed, Debug Codes: \n"
+    cout << "\n?? Failed, Debug Codes: ??\n"
 	 << "ms: " << ms << " mc: " << mc << " mj: " << mj << endl << endl;
 
 }
@@ -41,23 +61,26 @@ Int_t makeScripts(Int_t mode, Int_t pthatmin, Int_t pthatmax, Int_t maxRuns, con
   // For making scripts to use in running online
   char fName[100];
   char temp[200];
-  for(Int_t ii=0;ii<maxRuns;ii++)
+  for(Int_t ii=0;ii<maxRuns;ii++) // Loop to create the file for each run
     {
+      // create the file, naming convention has ptHat range and runnum
       if(mode == 1)
 	sprintf(fName, "%s/script/run_C%i_%i_%i.csh",folder, pthatmin,pthatmax,ii);
       if(mode == 2)
 	sprintf(fName, "%s/script/run_B%i_%i_%i.csh",folder, pthatmin,pthatmax,ii);
       std::ofstream outFile(fName,std::ofstream::out);
-      outFile << "#!/bin/csh" << endl << endl;
-      outFile << "source /star/u/zamiller/.cshrc" << endl;
-      outFile << "cd /star/u/zbtang/myTools/root/" << endl;
-      outFile << "source bin/thisroot.csh" << endl;
-      outFile << "cd /star/u/zamiller/simu/ptHatTemplates" << endl;
+
+      // Print the required lines
+      outFile << "#!/bin/csh" << endl << endl;                        // specify cShell
+      outFile << "source /star/u/zamiller/.cshrc" << endl;            // do csh login, sets $PATH to find PDFs and Pythia install
+      outFile << "cd /star/u/zbtang/myTools/root/" << endl;           // Use this version or root with TPythia
+      outFile << "source bin/thisroot.csh" << endl;                   // log in to root
+      outFile << "cd /star/u/zamiller/simu/ptHatTemplates" << endl;   // go to my folder
       if(mode==1)
 	sprintf(temp, "./NPEHDelPhiCorr cards/NpeC%i_%i_%i.cmnd output/npeC%i_%i/NpeCHcorr%i_%i_%i.root C",pthatmin,pthatmax,ii,pthatmin,pthatmax,pthatmin,pthatmax,ii);
       if(mode==2)
 	sprintf(temp, "./NPEHDelPhiCorr cards/NpeB%i_%i_%i.cmnd output/npeB%i_%i/NpeBHcorr%i_%i_%i.root B",pthatmin,pthatmax,ii,pthatmin,pthatmax,pthatmin,pthatmax,ii);
-      outFile << temp << endl;
+      outFile << temp << endl;                                        // Write the execution command. Options: card, output fileName, histogram Label
       outFile.close();
       }
   return 1;
@@ -69,13 +92,18 @@ Int_t makeJobs(Int_t mode, Int_t pthatmin, Int_t pthatmax, Int_t maxRuns, const 
   // For making jobs to submit online running
   char fName[100];
   char temp[200];
-  
+
+  // Open Files
   if(mode == 1)
     sprintf(fName, "%s/jobs/run_C%i_%i.job", folder,pthatmin,pthatmax);
   if(mode == 2)
     sprintf(fName, "%s/jobs/run_B%i_%i.job", folder,pthatmin,pthatmax);
   std::ofstream outfile(fName,std::ofstream::out);
+  
+  // This part is the same for all .job files. Only change the Inital Dir if new analysis directory created
   outfile << "Universe        = vanilla\nNotification    = never\nRequirements    = (CPU_Type != \"crs\") && (CPU_Experiment == \"star\")\nRank            = CPU_Speed\nPriority        = +19\nGetEnv          = True\nInitialdir      = /star/u/zamiller/simu/ptHatTemplates\nInput           = /dev/null\nNotify_user     = zamiller@rcf.rhic.bnl.gov\nGetEnv          = True\n+Experiment     = \"star\"\n+Job_Type       = \"cas\"\n\n";
+
+  // For each run and ptHat bin, tell it what to execute and where to put log files
   if(mode == 1)
     for(Int_t run=0; run < maxRuns; run++)
       {
@@ -111,7 +139,7 @@ Int_t makeJobs(Int_t mode, Int_t pthatmin, Int_t pthatmax, Int_t maxRuns, const 
 
 Int_t makeCards(Int_t mode, Float_t pthatmin, Float_t pthatmax, Int_t numRuns, const char* folder)
 {
-  
+  // make cards, naming convention ptHatRange and Runnum
   for(Int_t run=0; run < numRuns; run++){
     // open an output file
     char ch[1000];
@@ -122,9 +150,12 @@ Int_t makeCards(Int_t mode, Float_t pthatmin, Float_t pthatmax, Int_t numRuns, c
     
     std::ofstream outfile (ch);
     
+    // All of this stays the same for every ptbin, run, and template choice. Big block is ugly, but necessary.
     
     outfile << "#==============================================================================\n# STAR Heavy Flavor Tune 1.0\n#\n# PYTHIA Version 8.1.08\n# Date: September 9, 2008\n# Last updated by: Thomas Ullrich\n#\n# This file contains commands to be read in for a Pythia8 run. \n# Lines not beginning with a letter or digit are comments.\n# Names are case-insensitive  -  but spellings-sensitive!\n#==============================================================================\n\n#------------------------------------------------------------------------------\n# Parameters that need to be set by whoever runs it.\n# Note that they have no meaning unless restored and used\n# in the user provided code (main program).\n# This is not part of the star_hf tune (just convenient)\n# Documentation: <pyhiadir>/htmldoc/MainProgramSettings.html\n#------------------------------------------------------------------------------\nMain:numberOfEvents = 500000      ! number of events to generate\nMain:numberToList = 10              ! number of events to print\nMain:timesToShow = 1000            ! show how far along run is this many times\nMain:timesAllowErrors = 50000     ! abort run after this many flawed events\nMain:showChangedSettings = on      ! print changed flags/modes/parameters\nMain:showAllSettings = on          ! print all flags/modes/parameters\nMain:showAllStatistics = on        ! print statistics at the end\n\n#------------------------------------------------------------------------------\n# Colliding beams and center-of-mass energy\n# Documentation: <pyhiadir>/htmldoc/BeamParameters.html\n#------------------------------------------------------------------------------\nBeams:idA = 2212                  ! proton\nBeams:idB = 2212                  ! proton\nBeams:eCM = 200.                  ! RHIC nominal (GeV)\n\n#------------------------------------------------------------------------------\n# Process Selection\n# Make you selection by uncommenting the referring switches\n# \n# Warning: the b and c producing processes do not catch all possible \n# production modes. You would need to use HardQCD:all or even SoftQCD:minBias\n# for that. But the hard ones are the dominating ones and they are in.\n# Note that for pt -> 0 things might go very wrong. A lower pTHat cut avoids\n# this especially for charm and bottom production.\n# Documentation: <pyhiadir>/htmldoc/QCDProcesses.html\n# Documentation: <pyhiadir>/htmldoc/OniaProcesses.html\n#------------------------------------------------------------------------------\n# Uncomment for charmonium\n#Charmonium:all = on   ! charmonium production\n\n# Uncomment for charmonium singlet only\n# Charmonium:gg2QQbar[3S1(1)]g = on\n# Charmonium:gg2QQbar[3P0(1)]g = on\n# Charmonium:gg2QQbar[3P1(1)]g = on\n# Charmonium:gg2QQbar[3P2(1)]g = on\n# Charmonium:qg2QQbar[3P0(1)]q = on\n# Charmonium:qg2QQbar[3P1(1)]q = on\n# Charmonium:qg2QQbar[3P2(1)]q = on\n# Charmonium:qqbar2QQbar[3P0(1)]g = on\n# Charmonium:qqbar2QQbar[3P1(1)]g = on\n# Charmonium:qqbar2QQbar[3P2(1)]g = on\n\n# Uncomment for charmonium octett only\n# Charmonium:gg2QQbar[3S1(8)]g = on\n# Charmonium:gg2QQbar[1S0(8)]g = on\n# Charmonium:gg2QQbar[3PJ(8)]g = on\n# Charmonium:qg2QQbar[3S1(8)]q = on\n# Charmonium:qg2QQbar[1S0(8)]q = on\n# Charmonium:qg2QQbar[3PJ(8)]q = on\n# Charmonium:qqbar2QQbar[3S1(8)]g = on\n# Charmonium:qqbar2QQbar[1S0(8)]g = on\n# Charmonium:qqbar2QQbar[3PJ(8)]g = on\n\n# Uncomment for bottomonium\n# Bottomonium:all = on  ! bottomonium production\n\n# Uncomment for Drell-Yan\n# WeakSingleBoson:ffbar2gmZ = on\n\n# Hard processes main switch \n# HardQCD:all = on\n\n# Minimum bias \n# SoftQCD:minBias = on\n#------------------------------------------------------------------------------\n# K factor\n# Multiply almost all cross sections by this common fix factor.\n# This is usually no very useful. The data can be shifted up and down\n# later anyhow as we please. \n# Documentation: <pyhiadir>/htmldoc/CouplingsAndScales.html\n#------------------------------------------------------------------------------\n# SigmaProcess:Kfactor = 3\n\n#------------------------------------------------------------------------------\n# Scales (Ramona's suggestions)\n# This sets the scale to settings typically for hard probes:\n# mu_F = mu_R = 2*mT\n# Documentation: <pyhiadir>/htmldoc/CouplingsAndScales.html\n#------------------------------------------------------------------------------\nSigmaProcess:renormScale2 = 3\nSigmaProcess:factorScale2 = 3\nSigmaProcess:renormMultFac = 2   ! 2mT\nSigmaProcess:factorMultFac = 2   ! 2mT\n\n#------------------------------------------------------------------------------\n# PDF Selection:\n# Note: you need LHAPDF to be installed. Pythia 8 only provides a \n# minimal set to get started.\n# The choice of PDF here is greatly motivated by:\n# A.~Sherstnev and R.~S.~Thorne, arXiv:0807.2132 and arXiv:0711.2473v3\n# and W. Vogelsang (private communication)\n# These are PDFs especially made for LO Monte-Carlo generators such\n# as PYTHIA.\n# The state-of-the-art NLO PDF is cteq66.LHgrid which can be used\n# as an alternative (also from LHAPDF) but with the known problems\n# that arise when using a NLO PDF in an LO simulator.\n# Documentation: <pyhiadir>/htmldoc/PDFSelection.html\n#------------------------------------------------------------------------------\nPDF:useLHAPDF = on\nPDF:LHAPDFset = MRSTMCal.LHgrid\nPDF:extrapolateLHAPDF = on\n\n#------------------------------------------------------------------------------\n# Settings for the event generation process in the Pythia8 library\n# Effect/Relevance of MI, ISR and FSR need to be checked. For sure\n# the use more CPU and hence less events/s.\n# If HadronLevel:Hadronize = off we end up with the pure c, b spectra\n# (which might be useful at times)\n# Documentation: <pyhiadir>/htmldoc/MasterSwitches.html\n# Documentation: <pyhiadir>/htmldoc/MultipleInteractions.html\n#------------------------------------------------------------------------------\nPartonLevel:MI = on              ! multiple interactions\nPartonLevel:ISR = on             ! initial-state radiation \nBeamRemnants:primordialKT = on    ! primordial kt\nPartonLevel:FSR = on             ! final-state radiation\n#HadronLevel:Hadronize = off     ! no hadronization use\n\n#------------------------------------------------------------------------------\n# Relative production ratio vector/pseudoscalar for charm and bottom mesons\n# This was originally PARJ(13) where PARJ(13) = V/(PS+V) that is the \n# vector meson  fraction of primary charm+bottom mesons. \n# Andre David (CERN\nA60) made an exhaustive study and found that the\n# world data supports 0.6 while PYTHIA default was PARJ(13) = 3/4 = 0.75\n# from simple spin counting.\n# In PYTHIA8 we now use V/PS not V/(PS+V)\n# Documentation: <pyhiadir>/htmldoc/FlavourSelection.html\n#------------------------------------------------------------------------------\nStringFlav:mesonCvector = 1.5    ! same as PARJ(13)=0.6   -> 1.5\nStringFlav:mesonBvector = 3      ! leave at PARJ(13)=0.75 -> 3\n\n#------------------------------------------------------------------------------\n# Heavy quark masses.\n# Note that this should match with the ones used in the PDF.\n# The masses are listed in the header of the refering PDF file.\n# Documentation: <pyhiadir>/htmldoc/ParticleDataScheme.html\n# Documentation: <pyhiadir>/htmldoc/ParticleData.html\n#------------------------------------------------------------------------------\n4:m0 = 1.43\n5:m0 = 4.30\n\n#------------------------------------------------------------------------------# Particle Decay limits\n# When on, only particles with a decay within a volume limited by \n# rho = sqrt(x^2 + y^2) < xyMax and |z| < zMax are decayed. \n# The above xyMax, expressed in mm/c.\n#------------------------------------------------------------------------------\nParticleDecays:limitCylinder = on\nParticleDecays:xyMax = 600\nParticleDecays:zMax = 1000\n\nOther options below set in each card\n\n#------------------------------------------------------------------------------\n# To limit particle production to a certain pthat range uncomment\n# these lines. Use only when you 100% know what you are doing.\n# It is extremely useful to split runs up in ptHat bins to generate\n# statistics evenly in pt. Book keeping is important then (cross-sections,\n# number of events) to compile the final complete spectra.\n# Documentation: <pyhiadir>/htmldoc/PhaseSpaceCuts.html\n#------------------------------------------------------------------------------\n\n#------------------------------------------------------------------------------\n# Random Number\n# Initialize random generator according to time. Otherwise multiple jobs\n# will produce the same sequence (unless you pass a different seed every\n# time which is not practical).\n# Documentation: <pythiadir>/htmldoc/RandomNumberSeed.html\n#------------------------------------------------------------------------------\n"; // End put of standard options
 
+    // These are the options that change.
+    // turn on ccbar or bbar and turn off the opposite
     if(mode == 1)
       {
 	outfile << "\n# Uncomment next 2 lines for charm\n"
@@ -143,10 +174,12 @@ Int_t makeCards(Int_t mode, Float_t pthatmin, Float_t pthatmax, Int_t numRuns, c
 		<< "HardQCD:gg2bbbar = on    ! g g -> b bbar\n"
 		<< "HardQCD:qqbar2bbbar = on ! q qbar -> b bbar\n\n";
       }
-    
+
+    // Change ptHatRange
     outfile << "PhaseSpace:pTHatMin = " << pthatmin << "\n"
 	    << "PhaseSpace:pTHatMax = " << pthatmax << "\n";
-    
+
+    // Change RNG seed
     outfile << "Random:setSeed = on\n"
 	    << "Random:seed = " << 1000+run << "\n"
 	    << "#EOF";
